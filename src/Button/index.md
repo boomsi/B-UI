9b6dc11d7892a3f15e6cb599a4f44abcf55139bf
libo <libo@huanqiu.com>
2021-10-12 18:30:48 +0800

    feat:add button

--- src/Button/index.md
+++ src/Button/index.md
@@ -1,4 +1,3 @@
-
 ## Button
 
 Demo:
@@ -7,7 +6,19 @@ Demo:
 import React from 'react';
 import { Button } from 'bui';
 
-export default () => <Button text="First Demo" />;
+const style = {
+  marginRight: '8px',
+};
+
+export default () => (
+  <>
+    <Button style={style} text="Small" size="small" />
+    <Button style={style} text="Default" />
+    <Button text="Large" size="large" />
+    <br />
+    <Button text="Disabled" disabled />
+  </>
+);
 ```
 
 More skills for writing demo: https://d.umijs.org/guide/basic#write-component-demo
--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import cn from 'classnames';
+import { tuple } from '@/_util/type';
+import './index.less';
 
-interface IProps {
+const SizeTypes = tuple('small', 'default', 'large');
+const ButtonTypes = tuple('submit', 'button', 'reset');
+type SizeType = typeof SizeTypes[number];
+type ButtonType = typeof ButtonTypes[number];
+
+interface IButtonProps {
   text: string;
+  size?: SizeType;
+  style?: object;
+  htmlType?: ButtonType;
+  onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
-export default ({ text }: IProps) => <button>{text}</button>;
+type ButtonProps = IButtonProps &
+  Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'onClick'>;
+
+export default function Button({
+  text,
+  size = 'default',
+  className,
+  ...rest
+}: ButtonProps) {
+  const classNames = cn(
+    'b-btn',
+    {
+      'b-btn-sm': size === 'small',
+      'b-btn-lg': size === 'large',
+    },
+    className,
+  );
+
+  return (
+    <button className={classNames} {...(rest as ButtonProps)}>
+      <span>{text}</span>
+    </button>
+  );
+}
