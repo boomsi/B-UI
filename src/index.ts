d16d007603210dc85f770adcd26d5091014edf72
libo <libo@huanqiu.com>
2021-11-13 10:39:13 +0800

    fix:message subMenu show

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from './Button';
-export { default as message } from './Message';
+export { default as Message } from './Message';
 export { default as Upload } from './Upload';
