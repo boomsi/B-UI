d091a138f7faea4d7ac8af617b2d875a956789a7
libo <libo@huanqiu.com>
2021-11-18 15:38:42 +0800

    fix:message error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './Button';
-export { default as Message } from './Message';
+export { default as message } from './Message';
 export { default as Upload } from './Upload';
 export { default as Clock } from './Clock';
